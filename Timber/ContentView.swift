9bd71b33dc96a31a414012b475b7ec5697cd882b
Louyu000 <louyu50310@gmail.com>
2022-11-13 01:56:53 +0800

    Initial Commit

---
+++ Timber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timber
+//
+//  Created by User19 on 2022/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timber/TimberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimberApp.swift
+//  Timber
+//
+//  Created by User19 on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct TimberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
